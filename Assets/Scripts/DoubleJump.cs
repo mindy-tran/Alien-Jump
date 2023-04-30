7cf09d8ceb3e414aebf4181080b3b8d981896eb0
Mindy <mindyhtran@gmail.com>
2023-04-29 23:13:46 -0700

    Double Jump added

--- Assets/Scripts/DoubleJump.cs
+++ Assets/Scripts/DoubleJump.cs
@@ -1,22 +1,40 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
  
-// [RequireComponent(typeof(CharacterController))]
-// public class DoubleJump : MonoBehaviour
-// {
-//     private CharacterController controller;
-
-//     public float jumpSpeed = 2.0f;
-//     public float gravity = 10.0f;
-//     private Vector3 movingDirection = Vector3.zero;
-
-//     void Update() {
-//         if (controller.isGrounded && Input.GetButton("Jump"))
-//         {
-//             movingDirection.y = jumpSpeed;
-//         }
-//         movingDirection.y -= gravity * Time.deltaTime;
-//         controller.Move(movingDirection * Time.deltaTime);
-//     }
-// }
+[RequireComponent(typeof(Rigidbody))]
+public class DoubleJump : MonoBehaviour
+{
+    public float jumpForce = 5;
+    public float groundDistance = 0.5f;
+ 
+    Rigidbody rigidBody;
+    bool canDoubleJump;
+ 
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+ 
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundDistance);
+    }
+ 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(IsGrounded())
+            {
+                rigidBody.velocity = Vector3.up * jumpForce;
+                canDoubleJump = true;
+            }
+            else if(canDoubleJump)
+            {
+                rigidBody.velocity = Vector3.up * jumpForce;
+                canDoubleJump = false;
+            }
+        }
+    }
+}
