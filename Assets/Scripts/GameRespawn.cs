95e3829f819fbb08f523ef72dcf5fa27d620a872
Alexa Roskowski <aroskows@uoregon.edu>
2023-05-04 13:24:58 -0700

    added checkpoint flags

--- Assets/Scripts/GameRespawn.cs
+++ Assets/Scripts/GameRespawn.cs
@@ -3,9 +3,20 @@
  
  public class GameRespawn : MonoBehaviour {
      public float threshold;
+     
+     private Vector3 spawnpoint = new Vector3(0f,0f,0f);
  
      void FixedUpdate () {
          if (transform.position.y < threshold)
-             transform.position = new Vector3(0, 0, 0);
+             transform.position = spawnpoint;
      }
+
+     private void OnTriggerEnter(Collider other){
+			if(other.gameObject.CompareTag("Respawn"))
+            {
+                print("spawn set");
+                spawnpoint = transform.position;
+            }
+     }
+           
  }
