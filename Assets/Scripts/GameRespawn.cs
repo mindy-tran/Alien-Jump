ff459fe13927aa0c756e6dc3bf02c1e8c53b3a7c
Alexa Roskowski <aroskows@uoregon.edu>
2023-05-05 14:04:16 -0700

    made win flag checkered

--- Assets/Scripts/GameRespawn.cs
+++ Assets/Scripts/GameRespawn.cs
@@ -4,7 +4,7 @@
  public class GameRespawn : MonoBehaviour {
      public float threshold;
      
-     private Vector3 spawnpoint = new Vector3(0f,0f,0f);
+     private Vector3 spawnpoint = new Vector3(0f,0f,0f); //spawn on the center platform
  
      void FixedUpdate () {
          if (transform.position.y < threshold)
@@ -12,9 +12,8 @@
      }
 
      private void OnTriggerEnter(Collider other){
-			if(other.gameObject.CompareTag("Respawn"))
-            {
-                print("spawn set");
+			if(other.gameObject.CompareTag("Respawn")){
+                //character reached a checkpoint, change the spawnpoint 
                 spawnpoint = transform.position;
             }
      }
