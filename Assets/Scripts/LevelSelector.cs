a6563bd35909e4fef5acc3544ce1326a0d7f3ac5
Mindy <mindyhtran@gmail.com>
2023-05-19 22:22:04 -0700

    MainMenu implementation

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/* SUBSCRIBING TO MY YOUTUBE CHANNEL: 'VIN CODES' WILL HELP WITH MORE VIDEOS AND CODE SHARING IN THE FUTURE :) THANK YOU */
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+    // public Text levelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // levelText.text = level.ToString();
+    }
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Lvl " + level.ToString());
+    }
+
+}
